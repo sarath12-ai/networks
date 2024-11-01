58646d2f24d2502465bb4613ec0e4e18d1504f2a
Pyla <naidupyla04@gmail.com>
2024-11-01 21:34:45 +0530

    updated app structure

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,16 @@
 output "public_ip" {
-  description = "The public IP of the VM"
-  value       = azurerm_public_ip.public_ip.ip_address
+  value = azurerm_public_ip.pip.ip_address
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "acr_admin_username" {
+  value = azurerm_container_registry.acr.admin_username
+}
+
+output "acr_admin_password" {
+  value     = azurerm_container_registry.acr.admin_password
+  sensitive = true
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,28 +1,9 @@
-variable "subscription_id" {
-  description = "Azure Subscription ID"
-}
-
-variable "resource_group_name" {
-  description = "Resource group name"
-  default     = "myResourceGroup"
-}
-
-variable "location" {
-  description = "Azure region"
-  default     = "East US"
-}
-
-variable "vm_name" {
-  description = "Virtual machine name"
-  default     = "myVM"
-}
-
 variable "admin_username" {
-  description = "Admin username for the VM"
-  default     = "azureuser"
+  type    = string
+  default = "azureuser"
 }
 
-variable "ssh_public_key" {
-  description = "Path to SSH public key"
-  default     = "./ssh_key.pub"
+variable "admin_password" {
+  type      = string
+  sensitive = true
 }
