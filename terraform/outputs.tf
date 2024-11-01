5aa8575cb7b52546a133d761f37615bc0dbfc4f1
Pyla <naidupyla04@gmail.com>
2024-11-01 23:10:29 +0530

    Updated code

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,6 @@
-output "public_ip" {
-  value = azurerm_public_ip.pip.ip_address
-}
+# output "public_ip" {
+#   value = azurerm_public_ip.pip.ip_address
+# }
 
 output "acr_login_server" {
   value = azurerm_container_registry.acr.login_server
