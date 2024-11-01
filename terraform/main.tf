40285043f230e251c30e5b00daa075757311b40c
Pyla <naidupyla04@gmail.com>
2024-11-01 22:33:18 +0530

    Updated code

--- terraform/main.tf
+++ terraform/main.tf
@@ -84,7 +84,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "networkacrnpyla" # Update to a valid alphanumeric name
+  name                = "networkacrnaidupyla" # Update to a valid alphanumeric name
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
