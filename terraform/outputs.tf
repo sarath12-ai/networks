09dd4eb142f530eec864517966a8e392e201cb02
Pyla <naidupyla04@gmail.com>
2024-11-05 04:10:15 +0530

    No ACR anymore

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,15 +2,15 @@ output "public_ip" {
   value = azurerm_public_ip.pip.ip_address
 }
 
-output "acr_login_server" {
-  value = azurerm_container_registry.acr.login_server
-}
+# output "acr_login_server" {
+#   value = azurerm_container_registry.acr.login_server
+# }
 
-output "acr_admin_username" {
-  value = azurerm_container_registry.acr.admin_username
-}
+# output "acr_admin_username" {
+#   value = azurerm_container_registry.acr.admin_username
+# }
 
-output "acr_admin_password" {
-  value     = azurerm_container_registry.acr.admin_password
-  sensitive = true
-}
+# output "acr_admin_password" {
+#   value     = azurerm_container_registry.acr.admin_password
+#   sensitive = true
+# }
