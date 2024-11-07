73a1aeb0606237c94549a6f0733ad47aafe044f5
Pyla <naidupyla04@gmail.com>
2024-11-08 02:19:01 +0530

    updated

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,18 +5,18 @@ variable "admin_username" {
 
 variable "admin_password" {
   type      = string
-  sensitive = true
+   default = "P@ssw0rd12345"
 }
-variable "resource_group_name" {
-  description = "Name of the existing Azure Resource Group"
-  type        = string
-}
+# variable "resource_group_name" {
+#   description = "Name of the existing Azure Resource Group"
+#   type        = string
+# }
 
-variable "location" {
-  description = "Azure location of the existing Resource Group"
-  type        = string
-}
-variable "agent_public_ip" {
-  description = "The public IP of the DevOps agent running the pipeline"
-  type        = string
-}
+# variable "location" {
+#   description = "Azure location of the existing Resource Group"
+#   type        = string
+# }
+# variable "agent_public_ip" {
+#   description = "The public IP of the DevOps agent running the pipeline"
+#   type        = string
+# }
