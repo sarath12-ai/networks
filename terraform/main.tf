fe7248e3d697b50f82fbad39ccfbd87bd002ae90
Pyla <naidupyla04@gmail.com>
2024-11-10 18:55:48 +0530

    Key Pair Auth

--- terraform/main.tf
+++ terraform/main.tf
@@ -77,7 +77,8 @@ resource "azurerm_public_ip" "public_ip" {
   name                = "${var.vm_name}-public-ip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "static"
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
 # Network Interface
