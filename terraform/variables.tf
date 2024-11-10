eccfa9d5c7db782f1426da02059d6c33919d957c
Pyla <naidupyla04@gmail.com>
2024-11-10 18:18:22 +0530

    Key Pair Auth

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,22 +1,22 @@
-variable "admin_username" {
-  type    = string
-  default = "azureuser"
-}
-
-variable "admin_password" {
-  type      = string
-   default = "P@ssw0rd12345"
-}
-# variable "resource_group_name" {
-#   description = "Name of the existing Azure Resource Group"
-#   type        = string
+# variable "admin_username" {
+#   type    = string
+#   default = "azureuser"
 # }
 
-# variable "location" {
-#   description = "Azure location of the existing Resource Group"
-#   type        = string
+# variable "admin_password" {
+#   type      = string
+#    default = "P@ssw0rd12345"
 # }
-# variable "agent_public_ip" {
-#   description = "The public IP of the DevOps agent running the pipeline"
-#   type        = string
-# }
+# # variable "resource_group_name" {
+# #   description = "Name of the existing Azure Resource Group"
+# #   type        = string
+# # }
+
+# # variable "location" {
+# #   description = "Azure location of the existing Resource Group"
+# #   type        = string
+# # }
+# # variable "agent_public_ip" {
+# #   description = "The public IP of the DevOps agent running the pipeline"
+# #   type        = string
+# # }
