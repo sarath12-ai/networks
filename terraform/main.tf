482a8ad25b0b82d00789d25c5b69172b8526f37e
Pyla <naidupyla04@gmail.com>
2024-11-10 19:45:53 +0530

    Key Pair Auth

--- terraform/main.tf
+++ terraform/main.tf
@@ -95,6 +95,42 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
+resource "azurerm_network_security_group" "nsg" {
+  name                = "Network-NSG"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg_association" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
 # Linux Virtual Machine
 resource "azurerm_linux_virtual_machine" "linux_vm" {
   name                = var.vm_name
@@ -135,41 +171,7 @@ output "private_key" {
   sensitive = true
 }
 
-# resource "azurerm_network_security_group" "nsg" {
-#   name                = "Network-NSG"
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
 
-#   security_rule {
-#     name                       = "SSH"
-#     priority                   = 1001
-#     direction                  = "Inbound"
-#     access                     = "Allow"
-#     protocol                   = "Tcp"
-#     source_port_range          = "*"
-#     destination_port_range     = "22"
-#     source_address_prefix      = "*"
-#     destination_address_prefix = "*"
-#   }
-
-#   security_rule {
-#     name                       = "HTTP"
-#     priority                   = 1002
-#     direction                  = "Inbound"
-#     access                     = "Allow"
-#     protocol                   = "Tcp"
-#     source_port_range          = "*"
-#     destination_port_range     = "80"
-#     source_address_prefix      = "*"
-#     destination_address_prefix = "*"
-#   }
-
-# }
-
-# resource "azurerm_subnet_network_security_group_association" "nsg_association" {
-#   subnet_id                 = azurerm_subnet.subnet.id
-#   network_security_group_id = azurerm_network_security_group.nsg.id
-# }
 
 # resource "azurerm_virtual_network" "vnet" {
 #   name                = "Network-VNET"
