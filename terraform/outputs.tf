85be015c4bed6231f304b7837c89bbc9a1fef3d1
sarath12-ai <sarathpngd@gmail.com>
2024-12-02 10:02:55 +0000

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,7 +8,6 @@
 # # output "acr_admin_username" {
 # #   value = azurerm_container_registry.acr.admin_username
 # # }
-
 # # output "acr_admin_password" {
 # #   value     = azurerm_container_registry.acr.admin_password
 # #   sensitive = true
